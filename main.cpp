c0f9ca9f65f8a565b6f97c0ffc142581bd008f02
ggarredondo <ggarredondo@correo.ugr.es>
2021-03-26 14:10:56 +0100

    Lista de restricciones

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include "utilidades.h"
 
 int main() {
-    int_matrix test = archivo_a_matriz<int>(std::ifstream("data/bupa_set_const_10.const"));
-    for (size_t i = 0; i < test.size(); ++i) {
-        for (size_t j = 0; j < test[0].size(); ++j)
-            std::cout << test[i][j] << " ";
-        break;
-        std::cout << std::endl;
+    int_matrix matriz_r = archivo_a_matriz<int>(std::ifstream("data/bupa_set_const_10.const"));
+    R_list lista_r = matriz_a_lista(matriz_r);
+    size_t counter = 0;
+    for (auto i = lista_r.begin(); i != lista_r.end() && counter < 344; ++i) {
+        counter++;
+        std::cout << i->r << ", ";
     }
     return 0;
 }
--- utilidades.h
+++ utilidades.h
@@ -1,6 +1,8 @@
 #ifndef PRACTICA1_MH_PAR_UTILIDADES_H
 #define PRACTICA1_MH_PAR_UTILIDADES_H
 #include <vector>
+#include <list>
+#include <tuple>
 #include <fstream>
 
 typedef std::vector<std::vector<int> > int_matrix;
@@ -13,13 +15,13 @@ std::vector<std::vector<T> > archivo_a_matriz(std::ifstream archivo)
     std::vector<T> aux;
     std::string linea, number;
     while (archivo >> linea) {
-        for (auto i = linea.begin(); i != linea.end(); ++i) {
-            if (*i == ',') {
+        for (auto& i : linea) {
+            if (i == ',') {
                 aux.push_back(stod(number));
                 number.clear();
             }
             else
-                number.push_back(*i);
+                number.push_back(i);
         }
         aux.push_back(stod(number));
         number.clear();
@@ -27,6 +29,27 @@ std::vector<std::vector<T> > archivo_a_matriz(std::ifstream archivo)
         aux.clear();
     }
     return datos;
+}
+
+struct R {
+    size_t i, j;
+    int r;
+    R() : i(0), j(0), r(0) {}
+    R(size_t i, size_t j, int r) : i(i), j(j), r(r) {}
+};
+typedef std::list<R> R_list;
+
+R_list matriz_a_lista(int_matrix m) 
+{
+    R_list restricciones;
+    R rest;
+    for (size_t i = 0; i < m.size(); ++i) {
+        for (size_t j = i+1; j < m[i].size(); ++j) {
+            rest = R(i, j, m[i][j]);
+            restricciones.push_back(rest);
+        }
+    }
+    return restricciones;
 }
 
 #endif //PRACTICA1_MH_PAR_UTILIDADES_H
